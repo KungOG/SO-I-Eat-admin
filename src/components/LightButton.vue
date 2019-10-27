d0c62f2387c3438cf287f59d81bb24676f7fcaff
sandraottersten <otterstensandra@gmail.com>
2019-10-27 21:21:22 +0100

    added button component and clock

---
+++ src/components/LightButton.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="admin-btn">{{buttonText}}</button>   
+</template>
+
+<script>
+export default {
+    props: {
+        buttonText: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+    .admin-btn {
+        border: 1px solid white;
+        border-radius: 5px;
+        color: white;
+        background: none;
+        font-size: 16px; 
+        width: 65px;
+        height: 30px;
+        margin: 0 1rem;
+    }
+</style>
