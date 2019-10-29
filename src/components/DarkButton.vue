092a74998c5e2ca54f22b8d74ea1a3d38cc8819f
sandraottersten <otterstensandra@gmail.com>
2019-10-29 14:44:53 +0100

    added list and search bar

---
+++ src/components/DarkButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="admin-btn" :class="buttonText === productionTime ? 'active-btn' : '' ">{{buttonText}}</button>   
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    productionTime: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+  .admin-btn {
+    border: 1px solid black;
+    border-radius: 5px;
+    color: black;
+    background: none;
+    font-size: 16px; 
+    width: 65px;
+    height: 30px;
+    margin: 0;
+
+    &.active-btn {
+      background-color: white;
+      color: black;
+      outline: 0;
+    }
+  }
+</style>
--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -3,16 +3,33 @@
     <Navigation/>
     <div class="content-wrapper">
       <section>
-        <div class="search-bar">search bar</div>
+        <div class="search-bar">
+          <input type="text" v-model="search" placeholder="Sök">
+        </div>
         <div class="items-list">
           <ul>
-            <li v-for="(item, i) in menuItems" :key="i">{{ item.id }}{{ item.name }}</li>
+            <li v-for="(item, i) in filterMenuItems" :key="i"><span>{{ item.id }}</span>{{ item.name }}</li>
           </ul>        
         </div>
-        <div class="time-settings"></div>
+        <div class="time-settings">
+          <div>
+            <DarkButton :buttonText="'Öppen'"/>
+            <h3>Starttid</h3>
+            <h3>Stoptid</h3>
+          </div>
+          <div>
+            <DarkButton :buttonText="'Stängd'"/>
+            <DarkButton/>
+            <DarkButton/>
+          </div>
+        </div>
       </section>
       <section>
-        <h1>kjfkf</h1>
+        <div class="grid-wrapper">
+          <h1>Skapa ny menyrad</h1>
+          
+          <DarkButton />
+        </div>
       </section>
     </div>
   </div>
@@ -20,19 +37,30 @@
 
 <script>
 import Navigation from '@/components/NavigationBar.vue';
+import DarkButton from '@/components/DarkButton.vue';
 
 export default {
   name: 'edit',
   components: {
     Navigation,
+    DarkButton,
   },
+  data: () => ({
+    search: '',
+  }),
   beforeMount() {
-    this.$store.dispatch('getMenuItems')
+    this.$store.dispatch('getMenuItems');
   },
   computed: {
     menuItems() {
       return this.$store.state.menuItems;
+    },
+    filterMenuItems () {
+      return this.menuItems.filter((item) => {
+        return item.name.match(this.search);
+      })
     }
-  }
+  },
+  
 };
 </script>
