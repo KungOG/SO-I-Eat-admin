b8a573d47202c5bd370935fd577b7375d9990541
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-11-04 15:02:25 +0100

    FIX: filterMenu. KORR: Start on fix on our input fields and layout

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -8,7 +8,10 @@
         </div>
         <div class="items-list">
           <ul>
-            <li v-for="(item, i) in filterMenuItems" :key="i"><span>{{ item.productNr }}</span>{{ item.productName }}</li>
+            <li v-for="(item, i) in filterMenuItems" :key="`menuitems-${i}`" @click="showProduct(item._id)">
+              <span>{{ item.productNr }}</span>
+              {{ item.productName }}
+            </li>
           </ul>        
         </div>
         <div class="time-settings">
@@ -32,35 +35,57 @@
           <div class='wrapper'>
             <div class="number">
               <label>Nummer</label>
-              <input type="number" v-model="newProduct.productNr">
+              <input type="number" v-model.number.lazy="newProduct.productNr">
             </div>
             <div class="category">
               <label>Kategori</label>
-              <input type="number" v-model="newProduct.category">
+              <select v-model="newProduct.category">
+                <option disabled value="">Välj katergori</option>
+                <option type="number" value="0">Förätter</option>
+                <option type="number" value="1">Wokat</option>
+                <option type="number" value="2">Currygryta</option>
+              </select>
             </div>
             <div class="name">
               <label>Benämning</label>
-              <input type="text" v-model="newProduct.productName">
+              <input type="text" v-model.lazy="newProduct.productName">
             </div>
             <div class="price">
               <label>Pris</label>
-              <input type="number" v-model="newProduct.price">
+              <input type="number" v-model.number="newProduct.price">
             </div>
             <div class="protein">
-              <label>Protein</label>
-              <input type="text" v-model="newProduct.protein">
+              <span>Val av huvudingrediens</span>
+              <input type="checkbox" id="beef" value="Beef" v-model="newProduct.protein">
+              <label for="beef">Biff</label>
+              <input type="checkbox" id="chicken" value="Chicken" v-model="newProduct.protein">
+              <label for="chicken">Kyckling</label>
+              <input type="checkbox" id="pork" value="Pork" v-model="newProduct.protein">
+              <label for="pork">Fläsk</label>
+              <input type="checkbox" id="shrimp" value="Shrimp" v-model="newProduct.protein">
+              <label for="shrimp">Räkor</label>
+              <br>
             </div>
             <div class="description">
               <label>Beskrivning</label>
-              <input type="text" v-model="newProduct.description">
+              <textarea type="text" v-model="newProduct.description" />
             </div>
             <div class="ingredients">
               <label>Redigerbara ingredienser</label>
-              <input type="text" v-model="newProduct.ingredients">
+              <input type="text" v-model="newProduct.ingredients[0]">
+              <input type="text" v-model="newProduct.ingredients[1]">
+              <input type="text" v-model="newProduct.ingredients[2]">
+              <input type="text" v-model="newProduct.ingredients[3]">
+              <input type="text" v-model="newProduct.ingredients[4]">
             </div>
             <div class="spice">
-              <label>Justerbar styrka</label>
-              <input type="boolean" v-model="newProduct.spice">
+              <span>Justerbar styrka</span>
+              <input type="radio" id="yes" value="True" v-model="newProduct.spice">
+              <label for="yes">Ja</label>
+              <br>
+              <input type="radio" id="no" value="False" v-model="newProduct.spice">
+              <label for="no">Nej</label>
+              <br>
             </div>
           </div>
           <div class="button" @click="createNewProduct">
@@ -110,7 +135,20 @@ export default {
   },
   methods: {
     createNewProduct() {
-      this.$store.dispatch('createProduct', this.newProduct);
+      if(this.newProduct._id) {
+        this.$store.dispatch('updateProduct', this.newProduct);
+        console.log('UPDATE ---> ',this.newProduct);
+      } else {
+        this.$store.dispatch('createProduct', this.newProduct);
+        console.log('CREATE ---> ',this.newProduct);
+      }
+    },
+    showProduct(id) {
+      var productToEdit = this.menuItems.find(function(item) {
+        return item._id === id;
+      });
+      this.newProduct = productToEdit;
+      console.log(this.newProduct);
     }
   }
 };
