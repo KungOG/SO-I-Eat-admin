067e5052b80f9212dc2fd788c0e1075f9debbd18
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-11-04 10:28:54 +0100

    FIX: lint. Added links from our API

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,46 +1,29 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-   getOrders (ctx) {
-    const url = 'http://localhost:3000/orders';
-      axios
-    .get(url)
-    .then(response => {
-      ctx.commit('setOrders', response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    })
+  getOrders(ctx) {
+    const url = 'https://so-i-eat-server.herokuapp.com/orders';
+    axios
+      .get(url)
+      .then((response) => {
+        ctx.commit('setOrders', response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
-   getMenuItems (ctx) {
-    const url = 'http://localhost:3000/products';
-      axios
-    .get(url)
-    .then(response => {
-      ctx.commit('setMenuItems', response.data);
-    })
-    .catch(error => {
-      console.log(error);
-    })
+  getMenuItems(ctx) {
+    const url = 'https://so-i-eat-server.herokuapp.com/products';
+    axios
+      .get(url)
+      .then((response) => {
+        ctx.commit('setMenuItems', response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
-   async createProduct (ctx, newProduct) {
-    console.log(newProduct)
-    
-    await axios.post('http://localhost:3000/products', newProduct);
-    
-    console.log('funkade')
-
-
-
-
-    // const url = 'http://localhost:3000/products';
-    //   axios
-    // .post(url, newProduct)
-    // .then(response => {
-    //   console.log('jkjbkj')
-    // })
-    // .catch(error => {
-    //   console.log(error);
-    // })
+  async createProduct(ctx, newProduct) {
+    await axios.post('https://so-i-eat-server.herokuapp.com/products', newProduct);
   },
 };
