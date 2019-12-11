4fcdfcf065a2f8ae6826990015457c90726f7e65
sandraottersten <otterstensandra@gmail.com>
2019-12-11 12:12:30 +0100

    added auth0

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 import Vue from 'vue';
-
 import App from './App.vue';
 import router from './router';
-
 import store from './store/index';
+import { domain, clientId } from "../auth_config.json";
+import { Auth0Plugin } from "./auth";
 
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
 Vue.config.productionTip = false;
-
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,11 @@
 <template>
   <div class='login'>
+    <div v-if="!$auth.loading">
+      <!-- show login when not authenticated -->
+      <button v-if="!$auth.isAuthenticated" @click="login">Log in</button>
+      <!-- show logout when authenticated -->
+      <button v-if="$auth.isAuthenticated" @click="logout">Log out</button>
+    </div>
     <Modal v-if="showModal" @closeModal="showModal = false"/>
   </div>
 </template>
@@ -18,5 +24,17 @@ export default {
   mounted() {
     this.showModal = true;
   },
+  methods: {
+    // Log the user in
+    login() {
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
 };
 </script>
