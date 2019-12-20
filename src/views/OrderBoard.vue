398fd7a6526b902a1fb858054afd00b19009f450
Oscar Gustafsson <oscar-gustafsson@live.com>
2019-12-20 19:52:42 +0100

    FIX: eslint issue > Orderboard

--- src/views/OrderBoard.vue
+++ src/views/OrderBoard.vue
@@ -1,15 +1,22 @@
+
 <template>
   <div class='order-board'>
     <Navigation />
     <div class='content'>
       <div class='drink-content'>
-        <DrinkCard v-for="(order, i) in drinkOrders" :key="`drink-orders-${i}`" :order="order"/>
+        <DrinkCard v-for="(order, i) in drinkOrders"
+        :key="`drink-orders-${i}`"
+        :order="order"/>
       </div>
       <div class="food1">
-        <FoodCard v-for="(order, i) in foodOrders.array1" :key="`food-orders-array1-${i}`" :order="order" />
+        <FoodCard v-for="(order, i) in foodOrders.array1"
+        :key="`food-orders-array1-${i}`"
+        :order="order" />
       </div>
       <div class="food2">
-        <FoodCard v-for="(order, i) in foodOrders.array2" :key="`food-orders-array2-${i}`" :order="order" />
+        <FoodCard v-for="(order, i) in foodOrders.array2"
+        :key="`food-orders-array2-${i}`"
+        :order="order" />
       </div>
     </div>
     <OrderModal v-if="showModal"/>
@@ -17,9 +24,13 @@
 </template>
 
 <script>
+// eslint-disable-next-line import/no-unresolved
 import Navigation from '@/components/NavigationBar.vue';
+// eslint-disable-next-line import/no-unresolved
 import FoodCard from '@/components/Order/FoodCard.vue';
+// eslint-disable-next-line import/no-unresolved
 import DrinkCard from '@/components/Order/DrinkCard.vue';
+// eslint-disable-next-line import/no-unresolved
 import OrderModal from '@/components/OrderModal.vue';
 
 export default {
@@ -40,11 +51,11 @@ export default {
       return this.$store.state.showModal;
     },
     drinkOrders() {
-      let foodItems =  this.$store.getters.foodItems.map(x => x);
-      foodItems.sort(function(a, b) {
+      const foodItems = this.$store.getters.foodItems.map(x => x);
+      foodItems.sort(function (a, b) {
         if (a[1][0].date < b[1][0].date) return -1;
-        if (a[1][0].date > b[1][0].date) return 1; 
-        return 0;  
+        if (a[1][0].date > b[1][0].date) return 1;
+        return 0;
       })
       var arrayy = [];
       foodItems.forEach((x) => {
@@ -53,19 +64,19 @@ export default {
       return arrayy;
     },
     foodOrders() {
-      let foodItems =  this.$store.getters.foodItems.map(x => x);
-      let count =  this.$store.getters.foodItems;
-      var arrays = {array1: [], array2: []}
-      foodItems.sort(function(a, b) {
+      const foodItems = this.$store.getters.foodItems.map(x => x);
+      const count = this.$store.getters.foodItems;
+      const arrays = {array1: [], array2: []}
+      foodItems.sort(function (a, b) {
         if (a[1][0].date < b[1][0].date) return -1;
-        if (a[1][0].date > b[1][0].date) return 1; 
+        if (a[1][0].date > b[1][0].date) return 1;
         return 0;  
     })
       for(let i=0; i < count.length; i++) {
         foodItems.length !== 0 ? arrays.array1.push(foodItems.shift()) : '';
         foodItems.length !== 0 ? arrays.array2.push(foodItems.shift()) : '';
       }
-      return arrays
+      return arrays;
     },
   },
 };
