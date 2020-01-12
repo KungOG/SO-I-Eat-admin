42c07ca8b3f9d8689ecef3ba93eba4b08e1d74ab
Oscar Gustafsson <oscar-gustafsson@live.com>
2020-01-12 15:29:21 +0100

    fixed modal

--- src/views/OrderBoard.vue
+++ src/views/OrderBoard.vue
@@ -4,19 +4,25 @@
     <Navigation />
     <div class='content'>
       <div class='drink-content'>
-        <DrinkCard v-for="(order, i) in drinkOrders"
-        :key="`drink-orders-${i}`"
-        :order="order"/>
+        <DrinkCard
+          v-for="(order, i) in drinkOrders"
+          :key="`drink-orders-${i}`"
+          :order="order"
+        />
       </div>
       <div class="food1">
-        <FoodCard v-for="(order, i) in foodOrders.array1"
-        :key="`food-orders-array1-${i}`"
-        :order="order" />
+        <FoodCard
+          v-for="(order, i) in foodOrders.array1"
+          :key="`food-orders-array1-${i}`"
+          :order="order"
+        />
       </div>
       <div class="food2">
-        <FoodCard v-for="(order, i) in foodOrders.array2"
-        :key="`food-orders-array2-${i}`"
-        :order="order" />
+        <FoodCard
+          v-for="(order, i) in foodOrders.array2"
+          :key="`food-orders-array2-${i}`"
+          :order="order"
+        />
       </div>
     </div>
     <OrderModal v-if="showOrderModal"/>
