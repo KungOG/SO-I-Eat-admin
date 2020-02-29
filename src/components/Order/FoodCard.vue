75d952c72e5ecabe38463f06a072c15ace240396
Oscar Gustafsson <oscar-gustafsson@live.com>
2020-02-29 23:24:06 +0100

    Added comment icon and added it to foodcard

--- src/components/Order/FoodCard.vue
+++ src/components/Order/FoodCard.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class='food-order'>
-    <div class='food-header'>
-      <div class='filler' />
-      <span class='comment'
+  <div class="food-order">
+    <div class="food-header">
+      <div class="filler" />
+      <img
+        class="comment"
+        :src="comment"
         @click="toggleCommentModal"
         v-if="order[1][0].comment !== null"
-      >Kommentar</span>
-      <h2 v-if="order[1][0].orderInformation.table !== 'take away'">
-        Bord: {{order[1][0].orderInformation.table}}
-      </h2>
+      />
+      <h2
+        v-if="order[1][0].orderInformation.table !== 'take away'"
+      >Bord: {{order[1][0].orderInformation.table}}</h2>
       <h2 v-else>{{order[1][0].orderInformation.table}}</h2>
-      <span class='time'>{{time}}</span>
+      <span class="time">{{time}}</span>
     </div>
-    <div v-for="(orderItem, i) in order[1]"  :key="i">
+    <div v-for="(orderItem, i) in order[1]" :key="i">
       <FoodInformation
         v-for="(item, i) in orderItem.orderInformation.foodItems"
         :key="i"
@@ -24,66 +26,58 @@
         :table="order"
         :orderStatus="orderItem.status"
       />
-     </div>
-     <div
-      class='comment-modal'
-      v-if="showComment">
-      <div
-        class="filler"
-        @click="toggleCommentModal"
-      />
-      <div class='modal-content'>
-        <img
-          class='close-down-cross'
-          :src="whiteCross"
-          alt="Cancel icon"
-        />
-        <div
-          class='modal-content-wrapper'
-        >
-        <div class='modal-text'>
-          <span>ORDERNUMBER:</span>
-          <p>{{order[1][0].code}}</p>
-        </div>
-        <div class='modal-text'>
-          <span>KOMMENTAR:</span>
-          <p>{{order[1][0].comment}}</p>
-        </div>
+    </div>
+    <div class="comment-modal" v-if="showComment">
+      <div class="filler" @click="toggleCommentModal" />
+      <div class="modal-content">
+        <img class="close-down-cross" :src="whiteCross" alt="Cancel icon" />
+        <div class="modal-content-wrapper">
+          <div class="modal-text">
+            <span>ORDERNUMBER:</span>
+            <p>{{order[1][0].code}}</p>
+          </div>
+          <div class="modal-text">
+            <span>KOMMENTAR:</span>
+            <p>{{order[1][0].comment}}</p>
+          </div>
         </div>
       </div>
-     </div>
+    </div>
   </div>
 </template>
 
 <script>
-import FoodInformation from '@/components/Order/FoodInformation.vue';
-import WhiteCross from '@/assets/icons/WhiteCross.svg';
+import FoodInformation from "@/components/Order/FoodInformation.vue";
+import WhiteCross from "@/assets/icons/WhiteCross.svg";
+import Comment from "@/assets/icons/comment.svg";
 
 export default {
   props: {
     order: {
-      required: true,
-    },
+      required: true
+    }
   },
   data: () => ({
     whiteCross: WhiteCross,
-    showComment: false,
+    comment: Comment,
+    showComment: false
   }),
   components: {
     FoodInformation,
     WhiteCross,
+    Comment
   },
   computed: {
     time() {
       const date = new Date(this.order[1][0].date);
       const localTime = date.toLocaleTimeString().slice(0, 5);
       return localTime;
-    },
+    }
   },
   methods: {
     toggleCommentModal() {
       this.showComment = !this.showComment;
-    },
-  },
+    }
+  }
 };
 </script>
